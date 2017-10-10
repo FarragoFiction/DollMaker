680c107c76c6d0e37bfea75f22a3ce40d40543a3
Jenny <gtg287y@gmail.com>
2017-10-10 08:56:06 -0400

    aspect palettes

--- web/scripts/ReferenceColors.dart
+++ web/scripts/ReferenceColors.dart
@@ -115,12 +115,208 @@ abstract class ReferenceColours {
         ..pants_light = "#494949"
         ..pants_dark = "#393939";
 
+    static HomestuckPalette BLOOD = new HomestuckPalette()
+        ..accent = "#993300"
+        ..aspect_light = '#BA1016'
+        ..aspect_dark = '#820B0F'
+        ..shoe_light = '#381B76'
+        ..shoe_dark = '#1E0C47'
+        ..cloak_light = '#290704'
+        ..cloak_mid = '#230200'
+        ..cloak_dark = '#110000'
+        ..shirt_light = '#3D190A'
+        ..shirt_dark = '#2C1207'
+        ..pants_light = '#5C2913'
+        ..pants_dark = '#4C1F0D';
+
+
+    static HomestuckPalette BREATH = new HomestuckPalette()
+        ..accent = "#3399ff"
+        ..aspect_light = '#10E0FF'
+        ..aspect_dark = '#00A4BB'
+        ..shoe_light = '#FEFD49'
+        ..shoe_dark = '#D6D601'
+        ..cloak_light = '#0052F3'
+        ..cloak_mid = '#0046D1'
+        ..cloak_dark = '#003396'
+        ..shirt_light = '#0087EB'
+        ..shirt_dark = '#0070ED'
+        ..pants_light = '#006BE1'
+        ..pants_dark = '#0054B0';
+
+    static HomestuckPalette DOOM = new HomestuckPalette()
+        ..accent = "#003300"
+        ..aspect_light = '#0F0F0F'
+        ..aspect_dark = '#010101'
+        ..shoe_light = '#E8C15E'
+        ..shoe_dark = '#C7A140'
+        ..cloak_light = '#1E211E'
+        ..cloak_mid = '#141614'
+        ..cloak_dark = '#0B0D0B'
+        ..shirt_light = '#204020'
+        ..shirt_dark = '#11200F'
+        ..pants_light = '#192C16'
+        ..pants_dark = '#121F10';
+
+    static HomestuckPalette DREAM = new HomestuckPalette()
+        ..accent = "#9630BF"
+        ..aspect_light = '#cc87e8'
+        ..aspect_dark = '#9545b7'
+        ..shoe_light = '#ae769b'
+        ..shoe_dark = '#8f577c'
+        ..cloak_light = '#9630bf'
+        ..cloak_mid = '#693773'
+        ..cloak_dark = '#4c2154'
+        ..shirt_light = '#fcf9bd'
+        ..shirt_dark = '#e0d29e'
+        ..pants_light = '#bdb968'
+        ..pants_dark = '#ab9b55';
+
+    static HomestuckPalette HEART = new HomestuckPalette()
+        ..accent = "#ff3399"
+        ..aspect_light = '#BD1864'
+        ..aspect_dark = '#780F3F'
+        ..shoe_light = '#1D572E'
+        ..shoe_dark = '#11371D'
+        ..cloak_light = '#4C1026'
+        ..cloak_mid = '#3C0D1F'
+        ..cloak_dark = '#260914'
+        ..shirt_light = '#6B0829'
+        ..shirt_dark = '#4A0818'
+        ..pants_light = '#55142A'
+        ..pants_dark = '#3D0E1E';
+
+    static HomestuckPalette HOPE = new HomestuckPalette()
+        ..accent = "#ffcc66"
+        ..aspect_light = '#FDF9EC'
+        ..aspect_dark = '#D6C794'
+        ..shoe_light = '#164524'
+        ..shoe_dark = '#06280C'
+        ..cloak_light = '#FFC331'
+        ..cloak_mid = '#F7BB2C'
+        ..cloak_dark = '#DBA523'
+        ..shirt_light = '#FFE094'
+        ..shirt_dark = '#E8C15E'
+        ..pants_light = '#F6C54A'
+        ..pants_dark = '#EDAF0C';
+
+    static HomestuckPalette LIFE = new HomestuckPalette()
+        ..accent = "#494132"
+        ..aspect_light = '#76C34E'
+        ..aspect_dark = '#4F8234'
+        ..shoe_light = '#00164F'
+        ..shoe_dark = '#00071A'
+        ..cloak_light = '#605542'
+        ..cloak_mid = '#494132'
+        ..cloak_dark = '#2D271E'
+        ..shirt_light = '#CCC4B5'
+        ..shirt_dark = '#A89F8D'
+        ..pants_light = '#A29989'
+        ..pants_dark = '#918673';
+
+    static HomestuckPalette LIGHT = new HomestuckPalette()
+        ..accent = "#ff9933"
+        ..aspect_light = '#FEFD49'
+        ..aspect_dark = '#FEC910'
+        ..shoe_light = '#10E0FF'
+        ..shoe_dark = '#00A4BB'
+        ..cloak_light = '#FA4900'
+        ..cloak_mid = '#E94200'
+        ..cloak_dark = '#C33700'
+        ..shirt_light = '#FF8800'
+        ..shirt_dark = '#D66E04'
+        ..pants_light = '#E76700'
+        ..pants_dark = '#CA5B00';
+
+    static HomestuckPalette MIND = new HomestuckPalette()
+        ..accent = "#3da35a"
+        ..aspect_light = '#06FFC9'
+        ..aspect_dark = '#04A885'
+        ..shoe_light = '#6E0E2E'
+        ..shoe_dark = '#4A0818'
+        ..cloak_light = '#1D572E'
+        ..cloak_mid = '#164524'
+        ..cloak_dark = '#11371D'
+        ..shirt_light = '#3DA35A'
+        ..shirt_dark = '#2E7A43'
+        ..pants_light = '#3B7E4F'
+        ..pants_dark = '#265133';
+
+    static HomestuckPalette RAGE = new HomestuckPalette()
+        ..accent = "#9900cc"
+        ..aspect_light = '#974AA7'
+        ..aspect_dark = '#6B347D'
+        ..shoe_light = '#3D190A'
+        ..shoe_dark = '#2C1207'
+        ..cloak_light = '#7C3FBA'
+        ..cloak_mid = '#6D34A6'
+        ..cloak_dark = '#592D86'
+        ..shirt_light = '#381B76'
+        ..shirt_dark = '#1E0C47'
+        ..pants_light = '#281D36'
+        ..pants_dark = '#1D1526';
+
+    static HomestuckPalette SPACE = new HomestuckPalette()
+        ..accent = "#00ff00"
+        ..aspect_light = '#EFEFEF'
+        ..aspect_dark = '#DEDEDE'
+        ..shoe_light = '#FF2106'
+        ..shoe_dark = '#B01200'
+        ..cloak_light = '#2F2F30'
+        ..cloak_mid = '#1D1D1D'
+        ..cloak_dark = '#080808'
+        ..shirt_light = '#030303'
+        ..shirt_dark = '#242424'
+        ..pants_light = '#333333'
+        ..pants_dark = '#141414';
+
+    static HomestuckPalette TIME = new HomestuckPalette()
+        ..accent = "#ff0000"
+        ..aspect_light = '#FF2106'
+        ..aspect_dark = '#AD1604'
+        ..shoe_light = '#030303'
+        ..shoe_dark = '#242424'
+        ..cloak_light = '#510606'
+        ..cloak_mid = '#3C0404'
+        ..cloak_dark = '#1F0000'
+        ..shirt_light = '#B70D0E'
+        ..shirt_dark = '#970203'
+        ..pants_light = '#8E1516'
+        ..pants_dark = '#640707';
+
+    static HomestuckPalette VOID = new HomestuckPalette()
+        ..accent = "#000066"
+        ..aspect_light = '#0B1030'
+        ..aspect_dark = '#04091A'
+        ..shoe_light = '#CCC4B5'
+        ..shoe_dark = '#A89F8D'
+        ..cloak_light = '#00164F'
+        ..cloak_mid = '#00103C'
+        ..cloak_dark = '#00071A'
+        ..shirt_light = '#033476'
+        ..shirt_dark = '#02285B'
+        ..pants_light = '#004CB2'
+        ..pants_dark = '#003E91';
+
     static Map<String, Palette> _paletteList;
 
 
     static Map<String, Palette> get paletteList {
         if(_paletteList == null) {
             _paletteList = new Map<String, Palette>();
+            _paletteList["Blood"] = BLOOD;
+            _paletteList["Mind"] = MIND;
+            _paletteList["Rage"] = RAGE;
+            _paletteList["Void"] = VOID;
+            _paletteList["Time"] = TIME;
+            _paletteList["Heart"] = HEART;
+            _paletteList["Breath"] = BREATH;
+            _paletteList["Light"] = LIGHT;
+            _paletteList["Space"] = SPACE;
+            _paletteList["Hope"] = HOPE;
+            _paletteList["Life"] = LIFE;
+            _paletteList["Doom"] = DOOM;
+            _paletteList["Dream"] = DREAM;
             _paletteList["Robot"] = ROBOT_PALETTE;
             _paletteList["Prospit"] = PROSPIT_PALETTE;
             _paletteList["Derse"] = DERSE_PALETTE;
