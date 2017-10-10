a47704d5f050d7c2585bfa5fb7f26014289bb060
Jenny <gtg287y@gmail.com>
2017-10-09 22:41:59 -0400

    links added

--- web/scripts/Controllers/ContributeController.dart
+++ web/scripts/Controllers/ContributeController.dart
@@ -15,11 +15,20 @@ void main() {
 
 void printPalette() {
     Element paletteDiv = querySelector("#palette");
-    String html = "";
+    String html = "<table><tr><th><b>Trolls/Kids</b></th><th><b>Consorts</b></th></tr><td valign='top'>";
     List<String> names = new List<String>.from(ReferenceColours.TROLL_PALETTE.names);
     names.sort();
+
     for(String name in names) {
         html += "<li>$name: ${ReferenceColours.TROLL_PALETTE[name].toStyleString()}</li>";
     }
+    html += "</td><td valign='top'>";
+    names = new List<String>.from(ReferenceColours.CONSORT_PALETTE.names);
+    names.sort();
+
+    for(String name in names) {
+        html += "<li>$name: ${ReferenceColours.CONSORT_PALETTE[name].toStyleString()}</li>";
+    }
+    html+="</td></td></table>";
     paletteDiv.setInnerHtml(html);
 }
