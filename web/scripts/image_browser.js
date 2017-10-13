7ae5529815f22004ffd4db91f800ca39dc7ed16c
Jenny <gtg287y@gmail.com>
2017-10-13 00:29:36 -0400

    better visualizer

--- web/scripts/image_browser.js
+++ web/scripts/image_browser.js
@@ -153,7 +153,7 @@ function renderAllGlasses(){
 
 function renderAllBodies(){
 	var minHorn = 1
-	var maxHorn = 73;
+	var maxHorn = 97;
 	for(var i = minHorn; i<= maxHorn; i++){
 			renderLayeredSprites([new SpritePart("images/Homestuck/Body/"+i+".png","body "+i)]);
 	}
@@ -161,7 +161,7 @@ function renderAllBodies(){
 
 function renderAllSymbols(){
 	var minHorn = 1
-	var maxHorn = 148;
+	var maxHorn = 152;
 	for(var i = minHorn; i<= maxHorn; i++){
 			renderLayeredSprites([new SpritePart("images/Homestuck/Symbol/bg.png", ""),new SpritePart("images/Homestuck/Symbol/"+i+".png","symbol "+i)]);
 	}
