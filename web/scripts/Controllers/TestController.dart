fced7e0f8e18764410f8f85cefa33bca489d42a0
Jenny <gtg287y@gmail.com>
2017-10-15 10:31:45 -0400

    type isn't being loaded right. why not?

---
+++ web/scripts/Controllers/TestController.dart
@@ -0,0 +1,42 @@
+import "dart:html";
+import "../DollLib/DollRenderer.dart";
+import "../DollMakerTools.dart";
+import "../navbar.dart";
+
+Doll doll;
+
+void main() {
+    print("Hello World");
+    loadNavbar();
+    Random rand = new Random();
+    doll = new HomestuckDoll();
+    drawDollCreator();
+    setupForms();
+}
+
+
+void setupForms() {
+
+
+    ButtonElement loadButton = querySelector("#loadButton");
+    loadButton.onClick.listen((Event e) {
+        print('loading');
+        TextAreaElement dataBox = querySelector("#shareableURL");
+        //TODO if i'm given a url, chop it off till the ? do this in doll.
+        String dataString = dataBox.value;
+        doll = Doll.loadSpecificDoll(dataString);
+        drawDollCreator();
+    });
+
+}
+
+
+
+void drawDollCreator() {
+    print("Draw doll creator");
+
+    CanvasElement canvas = querySelector("#doll_creator");
+    Renderer.clearCanvas(canvas);
+    Renderer.drawDoll(canvas, doll,ReferenceColours.SPRITE_PALETTE);
+
+}
