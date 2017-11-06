4f86972820fa90fe844d0eedc6396cc81e899f93
Jenny <gtg287y@gmail.com>
2017-11-06 18:06:31 -0500

    really basic queen working, but can't be customized

---
+++ web/scripts/Controllers/QueenDollController.dart
@@ -0,0 +1,34 @@
+
+import "../HomestuckDollLib.dart";
+import "dart:html";
+import "../DollLib/DollRenderer.dart";
+import "../DollMakerTools.dart";
+import "../navbar.dart";
+import "BaseController.dart";
+
+BaseController controller;
+
+void main() {
+    print("Hello World");
+    loadNavbar();
+    Random rand = new Random();
+    controller = new BaseController(new QueenDoll());
+    print("going to load doll");
+    loadDoll();
+    //bundle means i don't have to preload shit
+    //Renderer.loadHomestuckDollParts(doll, loadDoll);
+
+}
+
+void loadDoll() {
+    print("loading doll");
+    String dataString = window.location.search;
+    print("dataSTring is $dataString");
+    if(dataString.isNotEmpty) {
+       controller.doll = new QueenDoll.fromDataString(dataString.substring(1)); //chop off leading ?
+    }
+
+    //whether i loaded or not, it's time to draw.
+    controller.setupForms();
+    controller.drawDollCreator();
+}
