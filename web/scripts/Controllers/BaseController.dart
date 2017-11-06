93e07089c433fba0465c5df914e4611df5893df3
Jenny <gtg287y@gmail.com>
2017-11-06 09:22:43 -0500

    dad parts

---
+++ web/scripts/Controllers/BaseController.dart
@@ -0,0 +1,71 @@
+
+import "../HomestuckDollLib.dart";
+import "dart:html";
+import "../DollLib/DollRenderer.dart";
+import "../DollMakerTools.dart";
+import "../navbar.dart";
+class BaseController {
+    Doll doll;
+
+    BaseController(this.doll);
+
+    void setupForms() {
+        querySelector("#randomize").onClick.listen((e) => randomizeDoll());
+        querySelector("#randomizeColors").onClick.listen((e) => randomizeDollColors());
+        querySelector("#randomizeNotColors").onClick.listen((e) => randomizeDollNotColors());
+
+        ButtonElement copyButton = querySelector("#copyButton");
+        copyButton.onClick.listen((Event e) {
+            TextAreaElement dataBox = querySelector("#shareableURL");
+            dataBox.select();
+            document.execCommand('copy');
+        });
+
+
+        Element layerControls = querySelector("#layerControls");
+        Element colorControls = querySelector("#colorControls");
+        for (SpriteLayer l in doll.renderingOrderLayers.reversed) {
+            DollMakerTools.drawDropDownForSpriteLayer(layerControls, l, drawDollCreator);
+        }
+        DollMakerTools.drawColorPickersForPallete(colorControls, doll.palette, drawDollCreator);
+    }
+
+
+    void drawDollCreator() {
+        print("Draw doll creator");
+        for (SpriteLayer l in doll.renderingOrderLayers) {
+            DollMakerTools.syncDropDownToSprite(l);
+        }
+
+        CanvasElement canvas = querySelector("#doll_creator");
+        Renderer.clearCanvas(canvas);
+        Renderer.drawDoll(canvas, doll);
+        TextAreaElement dataBox = querySelector("#shareableURL");
+        dataBox.value = "${window.location.origin}${window.location.pathname}?${doll.toDataBytesX()}";
+    }
+
+    void randomizeDoll() {
+        print("randomizing and redrawing");
+        doll.randomize();
+        //can't do it in regular draw part cuz onChange is a bitch.
+        DollMakerTools.syncColorPickersToSprite(doll.palette);
+        drawDollCreator();
+    }
+
+    void randomizeDollNotColors() {
+        print("randomizing and redrawing");
+        doll.randomizeNotColors();
+        //can't do it in regular draw part cuz onChange is a bitch.
+        DollMakerTools.syncColorPickersToSprite(doll.palette);
+        drawDollCreator();
+    }
+
+    void randomizeDollColors() {
+        print("randomizing and redrawing");
+        doll.randomizeColors();
+        //can't do it in regular draw part cuz onChange is a bitch.
+        DollMakerTools.syncColorPickersToSprite(doll.palette);
+        drawDollCreator();
+    }
+
+}
