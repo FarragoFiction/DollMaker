d63edd86b231dfa0d8c3fc543755f1d54bd709fe
Jenny <gtg287y@gmail.com>
2017-12-04 14:02:43 -0500

    undo/redo no longer infinite loops

--- web/scripts/Controllers/BaseController.dart
+++ web/scripts/Controllers/BaseController.dart
@@ -82,7 +82,7 @@ class BaseController {
             return;
         }
         doll = Doll.loadSpecificDoll(actionQueue[actionQueueIndex]);
-        drawDollCreator();
+        drawDollCreator(true);
     }
 
     void drawDollCreator([bool inQueue = false]) {
@@ -95,8 +95,9 @@ class BaseController {
         Renderer.drawDoll(canvas, doll);
         TextAreaElement dataBox = querySelector("#shareableURL");
         dataBox.value = "${window.location.origin}${window.location.pathname}?${doll.toDataBytesX()}";
-        actionQueue.add(doll.toDataBytesX());
+        //don't add it to the queue if you're already messing around in it, dunkass. you'll never escape the loop.
         if(!inQueue) {
+            actionQueue.add(doll.toDataBytesX());
             actionQueueIndex = actionQueue.length-1;
         }
     }
