7e9ae94d048251dc13915f41a2b481b2b0d2c861
Jenny <gtg287y@gmail.com>
2017-12-04 15:34:10 -0500

    trolls and kids all have the new mechanic too, and were dragged kicking and screaming into the new way of doing things

--- web/scripts/Controllers/HomestuckDollController.dart
+++ web/scripts/Controllers/HomestuckDollController.dart
@@ -3,27 +3,35 @@ import "../DollLib/DollRenderer.dart";
 import "../DollMakerTools.dart";
 import "../navbar.dart";
 
-HomestuckDoll doll;
-void makeEgg() {
-    Doll d = new EggDoll();
-    d.palette = doll.palette;
-    doll = d;
-}
+import "BaseController.dart";
+
+
+BaseController controller;
+
+
 void main() {
     print("Hello World");
     loadNavbar();
     Random rand = new Random();
-    doll = new HomestuckDoll();
+    Doll doll = new HomestuckDoll();
+    CanvasElement canvas = new CanvasElement(width: doll.width, height: doll.height);
+    querySelector("#doll").append(canvas);
+    controller = new BaseController(doll,canvas);
     print("going to load doll");
     loadDoll();
     hintAtEgg();
-
-
 
     //bundle means i don't have to preload shit
     //Renderer.loadHomestuckDollParts(doll, loadDoll);
 
 }
+
+void makeEgg() {
+    Doll d = new EggDoll();
+    d.palette = controller.doll.palette;
+    controller.doll = d;
+}
+
 
 void hintAtEgg() {
     AnchorElement a = new AnchorElement();
@@ -37,78 +45,14 @@ void loadDoll() {
     String dataString = window.location.search;
     print("dataSTring is $dataString");
     if(dataString.isNotEmpty) {
-       doll = new HomestuckDoll.fromDataString(dataString.substring(1)); //chop off leading ?
+        controller.doll = new HomestuckDoll.fromDataString(dataString.substring(1)); //chop off leading ?
     }
 
     //whether i loaded or not, it's time to draw.
-    setupForms();
-    drawDollCreator();
-}
-
-void setupForms() {
-    querySelector("#randomize").onClick.listen((e) => randomizeDoll());
-    querySelector("#randomizeColors").onClick.listen((e) => randomizeDollColors());
-    querySelector("#randomizeNotColors").onClick.listen((e) => randomizeDollNotColors());
-
-    ButtonElement copyButton = querySelector("#copyButton");
-    copyButton.onClick.listen((Event e) {
-        TextAreaElement dataBox = querySelector("#shareableURL");
-        dataBox.select();
-        document.execCommand('copy');
-    });
-
-    ButtonElement saveButton = querySelector("#saveButton");
-    saveButton.onClick.listen((Event e) {
-        doll.save();
-    });
-
-
-    Element layerControls = querySelector("#layerControls");
-    Element colorControls = querySelector("#colorControls");
-    Element samplePaletteControls = querySelector("#samplePaletteControls");
-    for(SpriteLayer l in doll.renderingOrderLayers.reversed) {
-        DollMakerTools.drawDropDownForSpriteLayer(doll, layerControls, l,drawDollCreator);
-    }
-    DollMakerTools.drawSamplePalettes(samplePaletteControls, doll, drawDollCreator);
-    DollMakerTools.drawColorPickersForPallete(colorControls, doll.palette, drawDollCreator);
-
+    controller.setupForms();
+    controller.drawDollCreator();
 }
 
 
 
-void drawDollCreator() {
-    print("Draw doll creator");
-    for(SpriteLayer l in doll.renderingOrderLayers) {
-        DollMakerTools.syncDropDownToSprite(l);
-    }
 
-    CanvasElement canvas = querySelector("#doll_creator");
-    Renderer.clearCanvas(canvas);
-    Renderer.drawDoll(canvas, doll);
-    TextAreaElement dataBox = querySelector("#shareableURL");
-    dataBox.value = "${window.location.origin}${window.location.pathname}?${doll.toDataBytesX()}";
-}
-
-void randomizeDoll() {
-    print("randomizing and redrawing");
-    doll.randomize();
-    //can't do it in regular draw part cuz onChange is a bitch.
-    DollMakerTools.syncColorPickersToSprite(doll.palette);
-    drawDollCreator();
-}
-
-void randomizeDollNotColors() {
-    print("randomizing and redrawing");
-    doll.randomizeNotColors();
-    //can't do it in regular draw part cuz onChange is a bitch.
-    DollMakerTools.syncColorPickersToSprite(doll.palette);
-    drawDollCreator();
-}
-
-void randomizeDollColors() {
-    print("randomizing and redrawing");
-    doll.randomizeColors();
-    //can't do it in regular draw part cuz onChange is a bitch.
-    DollMakerTools.syncColorPickersToSprite(doll.palette);
-    drawDollCreator();
-}
--- web/scripts/Controllers/HomestuckTrollDollController.dart
+++ web/scripts/Controllers/HomestuckTrollDollController.dart
@@ -1,23 +1,37 @@
-import "../HomestuckDollLib.dart";
 import "dart:html";
 import "../DollLib/DollRenderer.dart";
 import "../DollMakerTools.dart";
 import "../navbar.dart";
-HomestuckTrollDoll doll;
+
+import "BaseController.dart";
+
+
+BaseController controller;
 
 
 void main() {
     print("Hello World");
     loadNavbar();
     Random rand = new Random();
-    doll = new HomestuckTrollDoll();
-
+    Doll doll = new HomestuckTrollDoll();
+    CanvasElement canvas = new CanvasElement(width: doll.width, height: doll.height);
+    querySelector("#doll").append(canvas);
+    controller = new BaseController(doll,canvas);
+    print("going to load doll");
     loadDoll();
     hintAtEgg();
 
-    //don't need to preload anymore, thanks PL!
-    //Renderer.loadHomestuckTrollDollParts(doll, loadDoll);
+    //bundle means i don't have to preload shit
+    //Renderer.loadHomestuckDollParts(doll, loadDoll);
+
 }
+
+void makeEgg() {
+    Doll d = new TrollEggDoll();
+    d.palette = controller.doll.palette;
+    controller.doll = d;
+}
+
 
 void hintAtEgg() {
     AnchorElement a = new AnchorElement();
@@ -26,91 +40,19 @@ void hintAtEgg() {
     querySelector("#contents").append(a);
 }
 
-void makeEgg() {
-    Doll d = new TrollEggDoll();
-    d.palette = doll.palette;
-    doll = d;
-}
-
 void loadDoll() {
+    print("loading doll");
     String dataString = window.location.search;
     print("dataSTring is $dataString");
     if(dataString.isNotEmpty) {
-        doll = new HomestuckTrollDoll.fromDataString(dataString.substring(1)); //chop off leading ?
-
+        controller.doll = new HomestuckTrollDoll.fromDataString(dataString.substring(1)); //chop off leading ?
     }
+
     //whether i loaded or not, it's time to draw.
-    setupForms();
-    drawDollCreator();
-}
-
-void setupForms() {
-    querySelector("#randomize").onClick.listen((e) => randomizeDoll());
-    querySelector("#randomizeColors").onClick.listen((e) => randomizeDollColors());
-    querySelector("#randomizeNotColors").onClick.listen((e) => randomizeDollNotColors());
-
-    ButtonElement copyButton = querySelector("#copyButton");
-    copyButton.onClick.listen((Event e) {
-        TextAreaElement dataBox = querySelector("#shareableURL");
-        dataBox.select();
-        document.execCommand('copy');
-    });
-
-    ButtonElement saveButton = querySelector("#saveButton");
-    saveButton.onClick.listen((Event e) {
-        doll.save();
-    });
-
-
-    Element layerControls = querySelector("#layerControls");
-    Element colorControls = querySelector("#colorControls");
-    Element samplePaletteControls = querySelector("#samplePaletteControls");
-    for(SpriteLayer l in doll.renderingOrderLayers.reversed) {
-        DollMakerTools.drawDropDownForSpriteLayer(doll,layerControls, l,drawDollCreator);
-    }
-    DollMakerTools.drawSamplePalettes(samplePaletteControls, doll, drawDollCreator);
-
-    DollMakerTools.drawColorPickersForPallete(colorControls, doll.palette, drawDollCreator);
-
-
+    controller.setupForms();
+    controller.drawDollCreator();
 }
 
 
 
-void drawDollCreator() {
-    for(SpriteLayer l in doll.renderingOrderLayers) {
-        DollMakerTools.syncDropDownToSprite(l);
-    }
-    //DollMakerTools.syncColorPickersToSprite(doll.palette);
 
-    CanvasElement canvas = querySelector("#doll_creator");
-    Renderer.clearCanvas(canvas);
-    Renderer.drawDoll(canvas, doll);
-    TextAreaElement dataBox = querySelector("#shareableURL");
-    dataBox.value = "${window.location.origin}${window.location.pathname}?${doll.toDataBytesX()}";
-}
-
-void randomizeDoll() {
-    print("randomizing and redrawing");
-    doll.randomize();
-    //can't do it in regular draw part cuz onChange is a bitch.
-    DollMakerTools.syncColorPickersToSprite(doll.palette);
-    drawDollCreator();
-}
-
-
-void randomizeDollNotColors() {
-    print("randomizing and redrawing");
-    doll.randomizeNotColors();
-    //can't do it in regular draw part cuz onChange is a bitch.
-    DollMakerTools.syncColorPickersToSprite(doll.palette);
-    drawDollCreator();
-}
-
-void randomizeDollColors() {
-    print("randomizing and redrawing");
-    doll.randomizeColors();
-    //can't do it in regular draw part cuz onChange is a bitch.
-    DollMakerTools.syncColorPickersToSprite(doll.palette);
-    drawDollCreator();
-}
