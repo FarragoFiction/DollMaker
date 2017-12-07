41d2d6957dd4252bd253ca778245562a139f13dd
Jenny <gtg287y@gmail.com>
2017-12-07 13:51:26 -0500

    baby and grub maker because i got distracted

---
+++ web/scripts/Controllers/HomestuckGrubDollController.dart
@@ -0,0 +1,51 @@
+import "dart:html";
+import "../DollLib/DollRenderer.dart";
+import "../DollMakerTools.dart";
+import "../navbar.dart";
+
+import "BaseController.dart";
+
+
+BaseController controller;
+
+
+void main() {
+    print("Hello World");
+    loadNavbar();
+    Random rand = new Random();
+    Doll doll = new HomestuckGrubDoll();
+    CanvasElement canvas = new CanvasElement(width: doll.width, height: doll.height);
+    querySelector("#doll").append(canvas);
+    controller = new BabyController(doll,canvas);
+    print("going to load doll");
+    loadDoll();
+}
+
+
+void loadDoll() {
+    print("loading doll");
+    String dataString = window.location.search;
+    print("dataSTring is $dataString");
+    if(dataString.isNotEmpty) {
+        controller.doll = new HomestuckGrubDoll.fromDataString(dataString.substring(1)); //chop off leading ?
+    }
+
+    //whether i loaded or not, it's time to draw.
+    controller.setupForms();
+    controller.drawDollCreator();
+}
+
+
+class BabyController extends BaseController {
+    BabyController(Doll doll, CanvasElement canvas) : super(doll, canvas);
+
+    @override
+    void setupForms() {
+        super.setupForms();
+        Element samplePaletteControls = querySelector("#samplePaletteControls");
+        DollMakerTools.drawSamplePalettes(samplePaletteControls, controller, drawDollCreator);
+    }
+}
+
+
+
