176d940e1cfd30baec6d702a474980b4efa98244
Jenny <gtg287y@gmail.com>
2017-12-27 11:22:34 -0500

    if i'm going to have the saved dolls print out their edit linke (instead of making you make it yourself) then i need to finally get around to making a standardized builder page.

---
+++ web/scripts/Controllers/GenericDollController.dart
@@ -0,0 +1,71 @@
+import "dart:html";
+import "package:DollLibCorrect/DollRenderer.dart";
+import "../DollMakerTools.dart";
+import "../navbar.dart";
+
+import "BaseController.dart";
+
+
+BaseController controller;
+
+
+void main() {
+    print("Hello World");
+    loadNavbar();
+    Random rand = new Random();
+    Doll doll = new HomestuckDoll();
+    CanvasElement canvas = new CanvasElement(width: doll.width, height: doll.height);
+    querySelector("#doll").append(canvas);
+    controller = new KidController(doll,canvas);
+    print("going to load doll");
+    loadDoll();
+    //hintAtEgg();
+}
+
+void makeEgg() {
+    if(controller.doll is HomestuckDoll) {
+        Doll d = new EggDoll();
+        d.palette = controller.doll.palette;
+        controller.doll = d;
+    }else if(controller.doll is HomestuckTrollDoll) {
+        Doll d = new TrollEggDoll();
+        d.palette = controller.doll.palette;
+        controller.doll = d;
+    }
+}
+
+
+void hintAtEgg() {
+    AnchorElement a = new AnchorElement();
+    a.setInnerHtml("???");
+    a.onClick.listen((e) => makeEgg());
+    querySelector("#contents").append(a);
+}
+
+void loadDoll() {
+    print("loading doll");
+    String dataString = window.location.search;
+    print("dataSTring is $dataString");
+    if(dataString.isNotEmpty) {
+        controller.doll = Doll.loadSpecificDoll(dataString.substring(1)); //chop off leading ?
+    }
+
+    //whether i loaded or not, it's time to draw.
+    controller.setupForms();
+    controller.drawDollCreator();
+}
+
+
+class KidController extends BaseController {
+    KidController(Doll doll, CanvasElement canvas) : super(doll, canvas);
+
+    @override
+    void setupForms() {
+        super.setupForms();
+        Element samplePaletteControls = querySelector("#samplePaletteControls");
+        DollMakerTools.drawSamplePalettes(samplePaletteControls, controller, drawDollCreator);
+    }
+}
+
+
+
