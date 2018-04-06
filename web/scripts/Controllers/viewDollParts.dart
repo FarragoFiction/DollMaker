f539b192bbd935d4842f1222064124aaca095344
Jenny <gtg287y@gmail.com>
2018-04-06 14:28:51 -0400

    need to break for food, probably half way done with the parts viewer 2.0

--- web/scripts/Controllers/viewDollParts.dart
+++ web/scripts/Controllers/viewDollParts.dart
@@ -5,15 +5,21 @@ import "dart:async";
 
 
 List<Doll> dollExamples = new List<Doll>();
+List<CustomRadio> dollRadios = new List<CustomRadio>();
 DivElement div;
+Doll selectedDoll;
 void main() {
     loadNavbar();
     div = querySelector("#output");
     initDollList();
-    //drawAllBoxes();
-    todo("list of all doll types in a row");
+    todo("get selected Doll");
+    todo("display dolls palette automatically, both color and box");
     todo("drop down menu of each types parts Name: Number of Options");
     todo("when a part is selected, display all of them, min to max");
+    todo("take in a doll type for initial selection");
+    todo("have each doll maker or prt tester link to this page with doll type set");
+    drawAllBoxes();
+
 }
 
 void todo(String text) {
@@ -37,21 +43,53 @@ Future<Null> drawAllBoxes() async {
     }
 }
 
+void handleSelections() {
+    for(CustomRadio cr in dollRadios) {
+        cr.syncSelected();
+    }
+}
+
+void selectDoll(Doll doll) {
+    selectedDoll = doll;
+    todo("Print out palette for ${doll.name}");
+    todo("print out layers for ${doll.name}");
+    todo("have layers be clickable to show all images for that layer");
+}
+
 void drawBox(Doll doll) {
     DivElement box = new DivElement();
-    box.style.width = "${doll.width}";
-    AnchorElement a = new AnchorElement(href: "index.html?type=${doll.renderingType}");
-    box.append(a);
-    CanvasElement canvas = new CanvasElement(width:doll.width, height:doll.height);
+    box.style.display = "inline-block";
+    box.style.padding = "5px";
     box.style.border = "3px solid black";
-    canvas.style.marginTop = "10px";
-    canvas.style.border = "3px solid black";
-
     DivElement name = new DivElement();
     name.text = doll.name;
     box.append(name);
-    a.append(canvas);
     div.append(box);
-    //this is async but i don't care. i WANT it to not wait.
-    DollRenderer.drawDoll(canvas, doll);
+    RadioButtonInputElement radio = new RadioButtonInputElement();
+    radio.name = "DollRadioGroup";
+    box.append(radio);
+    dollRadios.add(new CustomRadio(box, radio));
+
+    box.onClick.listen((e)
+    {
+        radio.checked = true;
+        handleSelections();
+        selectDoll(doll);
+    });
+}
+
+class CustomRadio
+{
+    RadioButtonInputElement radioHidden;
+    Element radioVisible;
+
+    CustomRadio(Element this.radioVisible, RadioButtonInputElement this.radioHidden);
+
+    void syncSelected() {
+        if(radioHidden.checked) {
+            radioVisible.classes.add("selected");
+        }else {
+            radioVisible.classes.remove("selected");
+        }
+    }
 }
