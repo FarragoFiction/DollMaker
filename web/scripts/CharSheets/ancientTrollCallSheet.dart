f856f8ed78d49e5954d0be7af57bc13e95638a0e
Jenny <gtg287y@gmail.com>
2018-04-08 11:31:45 -0400

    trying to see why blank symbol dolls crash on the char sheets, but not locally

--- web/scripts/CharSheets/ancientTrollCallSheet.dart
+++ web/scripts/CharSheets/ancientTrollCallSheet.dart
@@ -43,9 +43,12 @@ class TrollCallSheetAncient extends CharSheet {
         for (SpriteLayer layer in doll.renderingOrderLayers) {
             if (layer.imgNameBase.contains("Symbol")) {
                 // print("found a symbol ${layer.imgLocation}");
-                foundSymbol = true;
-                await Renderer.drawWhateverFuture(cardElement, layer.imgLocation);
-                Renderer.swapPalette(cardElement, doll.paletteSource, doll.palette);
+                //if it's zero it tries to draw something clear to the end product and it messes up online???
+                if(layer.imgNumber != 0) {
+                    foundSymbol = true;
+                    await Renderer.drawWhateverFuture(cardElement, layer.imgLocation);
+                    Renderer.swapPalette(cardElement, doll.paletteSource, doll.palette);
+                }
             }
         }
         if (foundSymbol) {
