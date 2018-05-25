f456d53f322c05c01661d213edb35574aa2c44e5
Jenny <gtg287y@gmail.com>
2018-05-25 08:59:25 -0400

    can put your name/website/etc in

--- web/scripts/Controllers/UploadTest/UploadObject.dart
+++ web/scripts/Controllers/UploadTest/UploadObject.dart
@@ -6,6 +6,8 @@ class UploadObject {
     DivElement myElement;
     //usually will be one, but sometimes slaves/partners like for hair/horns
     List<SpriteLayer> layers;
+    InputElement creatorNameInput;
+    InputElement creatorNameWebsite;
 
     UploadObject(List<SpriteLayer> this.layers);
 
@@ -14,5 +16,24 @@ class UploadObject {
         myElement.classes.add("uploadElement");
         myElement.text = "${layers.join(",")}";
         container.append(myElement);
+        drawCreatorInput();
     }
+
+    void drawCreatorInput() {
+        DivElement creator = new DivElement();
+        myElement.append(creator);
+        LabelElement label = new LabelElement()..text = "How You Want To Be Credited:";
+        creatorNameInput = new InputElement();
+        creatorNameInput.classes.add("uploadInput");
+
+        LabelElement label2 = new LabelElement()..text = "Your Tumblr/Website/Etc:";
+        creatorNameWebsite = new InputElement();
+        creatorNameWebsite.classes.add("uploadInput");
+
+        creator.append(label);
+        creator.append(creatorNameInput);
+        creator.append(label2);
+        creator.append(creatorNameWebsite);
+    }
+
 }
