9ea20eec530820a509f30f1ffb3e6f6b92841a42
jadedResearcher <gtg287y@gmail.com>
2018-06-14 19:50:34 -0400

    fiddling with number of charms commonality

--- web/scripts/Shipping/Trove.dart
+++ web/scripts/Shipping/Trove.dart
@@ -50,6 +50,7 @@ class Trove {
             charmDiv = new DivElement();
             element.append(charmDiv);
         }
+        charmDiv.setInnerHtml(""); //wipe out any already drawn charms
         print("drawing participants");
         for(Charm c in charms) {
             c.draw(charmDiv);
@@ -58,11 +59,20 @@ class Trove {
 
 
     void setCharms() {
-        charms.clear(); //out with the old, make sure to always sync to dolls.
+        if(charms != null) charms.clear(); //out with the old, make sure to always sync to dolls.
         List<Charm> copyOfAllCharms = Charm.allCharms;
         Random rand = new Random(seed);
         copyOfAllCharms.shuffle(rand);
-        int i = rand.nextInt(13); //between zero and 13 charms
+        int i = 1;
+        //lower numbers are most common
+        for(int i = 0; i <13; i++) {
+            if(rand.nextDouble() < .3) {
+                i++;
+            }else {
+                break; //pl has taught me dangerous things
+            }
+        }
+
         i = Math.min(i, copyOfAllCharms.length); //don't be bigger than list
         charms = copyOfAllCharms.sublist(0,i);
         if(charmDiv != null) {
